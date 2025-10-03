ce575b50c25ccf5a573e4b59f119d2206353f342
Viki <vita152010@mail.ru>
2025-10-03 16:54:12 +0400

    added view

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,74 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <v-sheet
+    class="pa-4 text-center ma-4 mx-auto"
+    elevation="12"
+    max-width="1200"
+    rounded="lg"
+    width="100%"
+  >
+    <div class="d-flex align-center mb-4">
+      Учетные записи
+      <v-btn density="compact" icon="mdi-plus" class="ml-4"></v-btn>
+    </div>
+    <v-alert
+      text="Для указания нескольких меток для одной пары логин/пароль используйте разделитель ;"
+      type="info"
+      variant="tonal"
+    ></v-alert>
+
+    <v-list>
+      <v-list-item v-for="item in items" :key="item.id" :value="item.id">
+        <div class="item">
+          <v-text-field v-model="item.label" label="Метки" class="field"></v-text-field>
+          <v-autocomplete
+            v-model="item.type"
+            label="Тип записи"
+            :items="['Локальная', 'LDAP']"
+            class="field"
+          ></v-autocomplete>
+          <v-text-field v-model="item.login" label="Логин" class="field"></v-text-field>
+          <v-text-field
+            v-model="item.password"
+            label="Пароль"
+            :append-inner-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="showPassword ? 'text' : 'password'"
+            class="field"
+            @click:append-inner="showPassword = !showPassword"
+          ></v-text-field>
+
+          <v-btn density="compact" icon="mdi-delete" class="mt-3"></v-btn>
+        </div>
+      </v-list-item>
+    </v-list>
+  </v-sheet>
 </template>
 
-<style scoped></style>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const items = [{ label: 'XXX', type: 'Локальная', login: 'login', password: 'pass' }]
+const showPassword = ref(false)
+</script>
+
+<style lang="css" scoped>
+.field {
+  margin-right: 10px;
+}
+
+.item {
+  display: flex;
+  padding: 14px;
+}
+
+@media screen and (max-width: 600px) {
+  .item {
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .field {
+    margin-right: 0;
+    width: 100%;
+  }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,23 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 // Vuetify
 import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+  defaults: { VTextField: { variant: 'outlined' }, VAutocomplete: { variant: 'outlined' } },
 })
 
 const app = createApp(App)
